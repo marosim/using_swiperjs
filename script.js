ee5414dfc701b9be881bba0fcbc907193f9189a4
Tika <marosimarta1@gmail.com>
2021-01-17 23:22:24 +0100

    slides

---
+++ script.js
@@ -0,0 +1,19 @@
+function pageLoad() {
+
+  let swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+};
+
+window.addEventListener("load", pageLoad);
